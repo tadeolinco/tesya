dec0ee8d292daf71fea8cbfc61eb25513165a8a1
Samuel Bautista <tadeolinco@gmail.com>
2019-05-05 23:25:43 +0800

    Updates

---
+++ precache-manifest.54dd7baa5beedd6b211c2b3b439fdd07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a228797aaaedef7f0db366896d9123",
+    "url": "/tesya/index.html"
+  },
+  {
+    "revision": "987e90334a701c50279e",
+    "url": "/tesya/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "9774bdd7f67290e1d5fc",
+    "url": "/tesya/static/css/main.613975e6.chunk.css"
+  },
+  {
+    "revision": "987e90334a701c50279e",
+    "url": "/tesya/static/js/2.f247d8dd.chunk.js"
+  },
+  {
+    "revision": "9774bdd7f67290e1d5fc",
+    "url": "/tesya/static/js/main.ff6c9e8c.chunk.js"
+  },
+  {
+    "revision": "f16e71c7a3a0cdf67e17",
+    "url": "/tesya/static/js/runtime~main.17b6c3aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c90cd31d891f2b3d777491ecf39b7ae8.js"
+  "/tesya/precache-manifest.54dd7baa5beedd6b211c2b3b439fdd07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tesya/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
