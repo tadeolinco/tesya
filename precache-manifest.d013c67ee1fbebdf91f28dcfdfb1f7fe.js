85ea304af8dd6fa7316b4ce9488306bc894ee9ae
Samuel Bautista <tadeolinco@gmail.com>
2019-05-05 23:17:12 +0800

    Add build folder

---
+++ precache-manifest.d013c67ee1fbebdf91f28dcfdfb1f7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c50bf83396f46a19d0532fa03c3b6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987e90334a701c50279e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5f2ce1a7b4a7e5bfb05f",
+    "url": "/static/css/main.613975e6.chunk.css"
+  },
+  {
+    "revision": "987e90334a701c50279e",
+    "url": "/static/js/2.f247d8dd.chunk.js"
+  },
+  {
+    "revision": "5f2ce1a7b4a7e5bfb05f",
+    "url": "/static/js/main.8fcdd2dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
